4261f72e87084e9f33702464ee4c0d7bda551891
dangg004 <lequangdang1204@gmail.com>
2024-10-24 18:10:29 +0700

    Update StockRepository.cs

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
+using WebApplication1.DTOs.Stock;
 using WebApplication1.Interfaces;
 using WebApplication1.Models;
 
@@ -16,9 +18,54 @@ namespace WebApplication1.Repository
         {
             _context = context;
         }
-        public Task<List<Stock>> GetAllAsync()
+
+        public async Task<Stock> CreateAsync(Stock stockModel)
         {
-            return _context.Stock.ToListAsync();
+            await _context.Stock.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public async Task<Stock?> DeleteAsync(int ID)
+        {
+            var stockModel = await _context.Stock.FindAsync(ID);
+
+            if (stockModel == null) {
+                return null;
+            }
+
+            _context.Stock.Remove(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public async Task<List<Stock>> GetAllAsync()
+        {
+            return await _context.Stock.ToListAsync();
+        }
+
+        public async Task<Stock?> GetByIDAsync(int ID)
+        {
+            return await _context.Stock.FindAsync(ID);
+        }
+
+        public async Task<Stock?> UpdateAsync(int ID, UpdateStockRequestDTO updateDTO)
+        {
+            var stockModel = await _context.Stock.FindAsync(ID);
+
+            if (stockModel == null) {
+                return null;
+            }
+
+            stockModel.Symbol = updateDTO.Symbol;
+            stockModel.CompanyName = updateDTO.CompanyName;
+            stockModel.Industry = updateDTO.Industry;
+            stockModel.LastDiv = updateDTO.LastDiv;
+            stockModel.Purchase = updateDTO.Purchase;
+            stockModel.MarketCap = updateDTO.MarketCap;
+
+            await _context.SaveChangesAsync();
+            return stockModel;
         }
     }
 }
