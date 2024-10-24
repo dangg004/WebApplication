099c69be346d0b5db7028c8d675955df75bbcbd4
dangg004 <lequangdang1204@gmail.com>
2024-10-24 18:10:23 +0700

    Update IStockRepository.cs

--- Interfaces/IStockRepository.cs
+++ Interfaces/IStockRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.DTOs.Stock;
 using WebApplication1.Models;
 
 namespace WebApplication1.Interfaces
@@ -9,5 +10,9 @@ namespace WebApplication1.Interfaces
     public interface IStockRepository
     {
         Task<List<Stock>> GetAllAsync();
+        Task<Stock?> GetByIDAsync(int ID);
+        Task<Stock> CreateAsync(Stock stockModel);
+        Task<Stock?> UpdateAsync(int ID, UpdateStockRequestDTO stockDTO);
+        Task<Stock?> DeleteAsync(int ID);
     }
 }
