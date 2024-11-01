5bce157468485b40645ca0f1a7067ed4a1ed7dba
dangg004 <lequangdang1204@gmail.com>
2024-11-01 21:44:08 +0700

    Update CreateCommentDTO.cs

--- DTOs/Comment/CreateCommentDTO.cs
+++ DTOs/Comment/CreateCommentDTO.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,13 @@ namespace WebApplication1.DTOs.Comment
 {
     public class CreateCommentDTO
     {
+        [Required]
+        [MinLength(7, ErrorMessage = "Title must be at least 7 characters")]
+        [MaxLength(255, ErrorMessage = "Title cannot be over 255 characters")]
         public string Title { get; set; } = string.Empty;
+        [Required]
+        [MinLength(7, ErrorMessage = "Content must be at least 7 characters")]
+        [MaxLength(255, ErrorMessage = "Content cannot be over 255 characters")]
         public string Content { get; set; } = string.Empty;
     }
 }
