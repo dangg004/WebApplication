95dae79a69818b62bf3ff7749916cf6ec775464a
dangg004 <lequangdang1204@gmail.com>
2024-11-01 15:57:40 +0700

    Update CommentRepository.cs

--- Repository/CommentRepository.cs
+++ Repository/CommentRepository.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
+using WebApplication1.DTOs.Comment;
 using WebApplication1.Interfaces;
 using WebApplication1.Models;
 
@@ -16,6 +18,14 @@ namespace WebApplication1.Repository
         {
             _context = context;
         }
+
+        public async Task<Comment> CreateAsync(Comment commentModel)
+        {
+            await _context.Comments.AddAsync(commentModel);
+            await _context.SaveChangesAsync();
+            return commentModel;
+        }
+
         public async Task<List<Comment>> GetAllAsync()
         {
             return await _context.Comments.ToListAsync();
@@ -24,6 +34,18 @@ namespace WebApplication1.Repository
         public async Task<Comment?> GetByIDAsync(int ID)
         {
             return await _context.Comments.FindAsync(ID);
+        }
+
+        public async Task<Comment?> UpdateAsync(int ID, UpdateCommentRequestDTO commentDTO)
+        {
+            var existingComment = await _context.Comments.FindAsync(ID);
+            if (existingComment == null) {
+                return null;
+            }
+            existingComment.Title = commentDTO.Title;
+            existingComment.Content = commentDTO.Content;
+            await _context.SaveChangesAsync();
+            return existingComment;
         }
     }
 }
