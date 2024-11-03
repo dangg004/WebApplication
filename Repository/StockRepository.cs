cceb44bafb44d7c6c08e77bea1ea44f83bf29887
dangg004 <lequangdang1204@gmail.com>
2024-11-03 21:38:08 +0700

    Update StockRepository.cs

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -54,7 +54,8 @@ namespace WebApplication1.Repository
                     stock = query.IsDescending ? stock.OrderByDescending(s => s.Symbol) : stock.OrderBy(s => s.Symbol); 
                 }
             }
-            return await stock.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+            return await stock.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Stock?> GetByIDAsync(int ID)
