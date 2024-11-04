991c9f3e8964f21891b7ca3b4b697db36b80eff6
dangg004 <lequangdang1204@gmail.com>
2024-11-04 23:07:10 +0700

    Update ApplicationDBContext.cs

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Models;
@@ -16,5 +17,22 @@ namespace WebApplication1.Data
         }
         public DbSet<Stock> Stock { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole> {
+                new IdentityRole {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole {
+                    Name = "User",
+                    NormalizedName = "USER"
+                },
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
