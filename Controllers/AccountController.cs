15d3e80f63a7e176bcdb3f68c78f6cf6923b04f9
dangg004 <lequangdang1204@gmail.com>
2024-11-06 22:06:30 +0700

    Update AccountController.cs

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApplication1.DTOs.Account;
 using WebApplication1.Interfaces;
 using WebApplication1.Models;
@@ -16,10 +17,34 @@ namespace WebApplication1.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDTO loginDTO) {
+            if (!ModelState.IsValid) {
+                return BadRequest(ModelState);
+            }
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDTO.UserName.ToLower());
+            if (user == null) {
+                return Unauthorized("Username not found and/or password incorrect");
+            }
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDTO.Password, false);
+            if (!result.Succeeded) {
+                return Unauthorized("Username not found and/or password incorrect");
+            }
+            return Ok(
+                new NewUserDTO {
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    Token = _tokenService.CreateToken(user)
+                }
+            );
         }
 
         [HttpPost("register")]
