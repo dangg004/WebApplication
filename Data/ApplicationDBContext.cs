66192ad8b1868b8273f6a3c7a17bb7c77c000ef4
dangg004 <lequangdang1204@gmail.com>
2024-11-07 21:15:21 +0700

    Update ApplicationDBContext.cs

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -17,10 +17,23 @@ namespace WebApplication1.Data
         }
         public DbSet<Stock> Stock { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<Portfolio>(x => x.HasKey(p => new { p.AppUserID, p.StockID }));
+
+            builder.Entity<Portfolio>()
+                .HasOne(x => x.AppUser)
+                .WithMany(x => x.Portfolios)
+                .HasForeignKey(p => p.AppUserID);
+            
+            builder.Entity<Portfolio>()
+                .HasOne(x => x.Stock)
+                .WithMany(x => x.Portfolios)
+                .HasForeignKey(p => p.StockID);
 
             List<IdentityRole> roles = new List<IdentityRole> {
                 new IdentityRole {
