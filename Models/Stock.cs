11727a68e66803cc87b39eb6975a7ba9e01b9d4f
dangg004 <lequangdang1204@gmail.com>
2024-11-07 21:15:55 +0700

    Update Stock.cs

--- Models/Stock.cs
+++ Models/Stock.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace WebApplication1.Models
 {
+    [Table("Stocks")]
     public class Stock
     {
         public int ID { get; set; }
@@ -18,5 +19,7 @@ namespace WebApplication1.Models
         public string Industry { get; set; } = string.Empty;
         public long MarketCap { get; set; }
         public List<Comment> Comments { get; set; } = new List<Comment>();
+        public List<Portfolio> Portfolios { get; set; } = new List<Portfolio>();
+
     }
 }
