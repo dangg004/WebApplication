ad0153db527b988565256c1f104b9cc70f92005f
dangg004 <lequangdang1204@gmail.com>
2024-11-09 23:10:54 +0700

    Create 20241109134500_CommentOneToOne.cs

---
+++ Migrations/20241109134500_CommentOneToOne.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class CommentOneToOne : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "17d0071e-621f-430f-a8f1-048b3f67e346");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2f19ec4e-de86-4f31-bae5-79468d567be8");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AppUserID",
+                table: "Comments",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "0a2fb12f-0d23-4095-8331-2e3492f51e25", null, "User", "USER" },
+                    { "62ed7f50-d433-4ee2-83a0-882a75c1ad8a", null, "Admin", "ADMIN" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_AppUserID",
+                table: "Comments",
+                column: "AppUserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_AppUserID",
+                table: "Comments",
+                column: "AppUserID",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_AppUserID",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_AppUserID",
+                table: "Comments");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0a2fb12f-0d23-4095-8331-2e3492f51e25");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "62ed7f50-d433-4ee2-83a0-882a75c1ad8a");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserID",
+                table: "Comments");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "17d0071e-621f-430f-a8f1-048b3f67e346", null, "User", "USER" },
+                    { "2f19ec4e-de86-4f31-bae5-79468d567be8", null, "Admin", "ADMIN" }
+                });
+        }
+    }
+}
