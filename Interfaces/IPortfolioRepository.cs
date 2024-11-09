dddb05f8e5de73c9a6129de52d3dc60de527e03c
dangg004 <lequangdang1204@gmail.com>
2024-11-09 20:59:27 +0700

    Update IPortfolioRepository.cs

--- Interfaces/IPortfolioRepository.cs
+++ Interfaces/IPortfolioRepository.cs
@@ -10,5 +10,6 @@ namespace WebApplication1.Interfaces
     {
         Task<List<Stock>> GetUserPortfolio(AppUser user);
         Task<Portfolio> CreateAsync(Portfolio portfolio);
+        Task<Portfolio> DeleteAsync(AppUser appUser, string symbol);
     }
 }
