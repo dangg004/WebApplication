d19a7b8ee429cd4528a4005a5ee27fac8783e65b
dangg004 <lequangdang1204@gmail.com>
2024-11-13 21:10:24 +0700

    Update CommentDTO.cs

--- DTOs/Comment/CommentDTO.cs
+++ DTOs/Comment/CommentDTO.cs
@@ -11,6 +11,7 @@ namespace WebApplication1.DTOs.Comment
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public string CreatedBy { get; set; } = string.Empty;
         public int? StockID { get; set; }
     }
 }
