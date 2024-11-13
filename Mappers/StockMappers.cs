254ca4bb17968d6af2c8354d15d089f87f3ed6aa
dangg004 <lequangdang1204@gmail.com>
2024-11-13 23:05:30 +0700

    Update StockMappers.cs

--- Mappers/StockMappers.cs
+++ Mappers/StockMappers.cs
@@ -32,5 +32,16 @@ namespace WebApplication1.Mappers
                 MarketCap = stockDTO.MarketCap
             };
         }
+
+        public static Stock ToStockFromFMP(this FMPStock fmpStock) {
+            return new Stock {
+                Symbol = fmpStock.symbol,
+                CompanyName = fmpStock.companyName,
+                Purchase = (decimal)fmpStock.price,
+                LastDiv = (decimal)fmpStock.lastDiv,
+                Industry = fmpStock.industry,
+                MarketCap = fmpStock.mktCap
+            };
+        }
     }
 }
