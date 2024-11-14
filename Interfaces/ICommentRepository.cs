bdcc8a9bcd490a5d574bf45e610c9d70df07aff2
dangg004 <lequangdang1204@gmail.com>
2024-11-14 20:55:41 +0700

    Update ICommentRepository.cs

--- Interfaces/ICommentRepository.cs
+++ Interfaces/ICommentRepository.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication1.DTOs.Comment;
+using WebApplication1.Helpers;
 using WebApplication1.Models;
 
 namespace WebApplication1.Interfaces
 {
     public interface ICommentRepository
     {
-        Task<List<Comment>> GetAllAsync();
+        Task<List<Comment>> GetAllAsync(CommentQueryObject queryObject);
         Task<Comment?> GetByIDAsync(int ID);
         Task<Comment> CreateAsync(Comment commentModel);
         Task<Comment?> UpdateAsync(int ID, UpdateCommentRequestDTO commentDTO);
