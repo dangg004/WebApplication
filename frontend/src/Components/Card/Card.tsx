cde5c8fce64b92a3e35764ee138952744b171980
dangg004 <lequangdang1204@gmail.com>
2024-11-18 22:40:33 +0700

    Update Card.tsx

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,25 +1,34 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import "./Card.css";
 import { CompanySearch } from "../../company";
+import AddPortfolio from "../Portfolio/AddPortfolio/AddPortfolio";
 
 interface Props {
   id: string;
   searchResult: CompanySearch;
+  onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
-const Card: React.FC<Props> = ({ id, searchResult }: Props): JSX.Element => {
+const Card: React.FC<Props> = ({
+  id,
+  searchResult,
+  onPortfolioCreate,
+}: Props): JSX.Element => {
   return (
-    <div className="card">
-      <img alt="Company logo" />
+    <div key={id} id={id} className="card">
       <div className="details">
         <h2>
           {searchResult.name} ({searchResult.symbol})
         </h2>
-        <p>${searchResult.currency}</p>
+        <p>{searchResult.currency}</p>
       </div>
       <p className="info">
         {searchResult.exchangeShortName} - {searchResult.stockExchange}
       </p>
+      <AddPortfolio
+        onPortfolioCreate={onPortfolioCreate}
+        symbol={searchResult.symbol}
+      />
     </div>
   );
 };
