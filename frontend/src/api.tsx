6a48a254a4b489cbd278e318379639467f6cbe04
dangg004 <lequangdang1204@gmail.com>
2024-11-22 12:01:00 +0700

    Update api.tsx

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import {
   CompanyBalanceSheet,
   CompanyCashFlow,
+  CompanyCompData,
   CompanyIncomeStatement,
   CompanyKeyMetrics,
   CompanyProfile,
@@ -76,6 +77,17 @@ export const getCashFlowStatement = async (query: string) => {
   try {
     const data = await axios.get<CompanyCashFlow[]>(
       `https://financialmodelingprep.com/api/v3/cash-flow-statement/${query}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("Error message from API: ", error.message);
+  }
+};
+
+export const getCompData = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyCompData[]>(
+      `https://financialmodelingprep.com/api/v4/stock_peers?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
     );
     return data;
   } catch (error: any) {
