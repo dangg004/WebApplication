354dc4ae855b468f01aa93e69967cf9bb26b2936
dangg004 <lequangdang1204@gmail.com>
2024-11-23 11:41:27 +0700

    Update company.d.ts

--- frontend/src/company.d.ts
+++ frontend/src/company.d.ts
@@ -313,3 +313,13 @@ export interface CompanyCompData {
   symbol: string;
   peersList: string[];
 }
+
+export interface Company10K {
+  symbol: string;
+  fillingDate: string;
+  acceptedDate: string;
+  cik: string;
+  type: string;
+  link: string;
+  finalLink: string;
+}
