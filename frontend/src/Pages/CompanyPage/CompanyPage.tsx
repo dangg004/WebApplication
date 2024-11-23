bbf708595db9671cacd5e341850ad093db8a167c
dangg004 <lequangdang1204@gmail.com>
2024-11-23 11:50:47 +0700

    Update CompanyPage.tsx

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -7,6 +7,7 @@ import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard
 import Tile from "../../Components/Tile/Tile";
 import Spinner from "../../Components/Spinner/Spinner";
 import CompFinder from "../../Components/CompFinder/CompFinder";
+import TenKFinder from "../../Components/TenKFinder/TenKFinder";
 
 interface Props {}
 
@@ -29,13 +30,17 @@ const CompanyPage = (props: Props) => {
           <Sidebar />
           <CompanyDashboard ticker={ticker!}>
             <Tile title="Company Name" subTitle={company.companyName}></Tile>
-            <Tile title="Price" subTitle={company.price.toString()}></Tile>
-            <Tile title="Sector" subTitle={company.sector}></Tile>
             <Tile
-              title="Market Cap"
-              subTitle={company.mktCap.toString()}
+              title="Price"
+              subTitle={"$" + company.price.toString()}
             ></Tile>
+            <Tile title="DCF" subTitle={"$" + company.dcf.toString()}></Tile>
+            <Tile title="Sector" subTitle={company.sector}></Tile>
             <CompFinder ticker={company.symbol} />
+            <TenKFinder ticker={company.symbol} />
+            <p className="bg-white shadow rounded text-medium font-medium text-gray-900 p-3 mt-1 m-4">
+              {company.description}
+            </p>
           </CompanyDashboard>
         </div>
       ) : (
